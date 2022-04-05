d52f319664c0c1b2a2ad019ea79cae58a7fd2299
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-04-05 17:49:07 -0400

    Adding saveBtn, colored text area and localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,62 @@
 var container = document.getElementById("container");
-// currentDay.textContent = moment().format('LLLL');
 
 window.setInterval(function () {
   $('#currentDay').html(moment().format('dddd MM/DD/YYYY H:mm:ss'))
 }, 1000);
 
-
-
 var hours = [
-  // moment().format("1"),
-  // moment().format("2"),
-  // moment().format("3"),
-  // moment().format("4"),
-  // moment().format("5"),
-  // moment().format("6"),
-  // moment().format("7"),
-  // moment().format("8"),
-  // moment().format("9"),
-  // moment().format("10"),
-  // moment().format("11"),
-  // moment().format("12"),
-  // moment().format("13"),
-  // moment().format("14"),
+  moment().format("9"),
+  moment().format("10"),
+  moment().format("11"),
+  moment().format("12"),
+  moment().format("13"),
+  moment().format("14"),
   moment().format("15"),
   moment().format("16"),
   moment().format("17"),
-  moment().format("18"),
-  moment().format("19"),
-  moment().format("20"),
-  moment().format("21"),
-  moment().format("22"),
-  moment().format("23"),
 ];
-
-console.log(moment.now());
 
 hours.forEach((hour) => {
   var row = document.createElement("div");
   row.classList.add("row", "time-block");
   row.innerHTML = `
-    <div id="${hour}" class="col-md-2 hour">${hour}</div>
-    <textarea class="col-md-9 description" name="text-area-${hour}" id="${hour}"></textarea>
+    <div id="${hour}" class="col-md-1 hour">${hour}</div>
+    <textarea class="col-md-10 description" id="text-area-${hour}"></textarea>
     <button data-id="${hour}"  class="col-md-1 btn saveBtn"><i class="fas fa-save"></i></button>
   </div>`;
   container.appendChild(row);
 });
 
-$(".hour").each(function () {
-  var timeBlock = $(".hour");
+$(".time-block").each(function () {
+  var timeBlock = parseInt(this.children[0].id);
   var now = moment().hours();
-  // console.log(timeBlock);
-  // console.log(now);
-  // if (now === ".hour") ({
-  //   timeBlock.classList.add(".present")
-  // }
+  $(this).addClass("past")
+  console.log(timeBlock)
+  if (now === timeBlock) {
+    $(this).addClass("present")
+  }
+  if (now > timeBlock) {
+    $(this).addClass("past")
+  }
+  if (now < timeBlock) {
+    $(this).addClass("future")
+  }
 });
 
-// function hourComparison() {
-//   console.log(now);
-//   if (hours.hour ) {
-//   }
-// }
+$(".saveBtn").on("click", function () {
+  console.log(this)
+  console.log($(this).attr("data-id"))
+  var value = $(this).siblings(".description").val()
+  var key = $(this).attr("data-id")
+  localStorage.setItem(key, value)
+})
 
-// x = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
-// y = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
+$("#text-area-9").val(localStorage.getItem("9"))
+$("#text-area-10").val(localStorage.getItem("10"))
+$("#text-area-11").val(localStorage.getItem("11"))
+$("#text-area-12").val(localStorage.getItem("12"))
+$("#text-area-13").val(localStorage.getItem("13"))
+$("#text-area-14").val(localStorage.getItem("14"))
+$("#text-area-15").val(localStorage.getItem("15"))
+$("#text-area-16").val(localStorage.getItem("16"))
+$("#text-area-17").val(localStorage.getItem("17"))
